a9c4c0c20bde3f213fec62135b3462471bce2e9e
lsj8367 <lsj8367@naver.com>
2023-02-26 11:45:46 +0900

    :sparkles: Initial Commit!

---
+++ src/main/kotlin/com/kata/board/BoardKataApplication.kt
@@ -0,0 +1,11 @@
+package com.kata.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardKataApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardKataApplication>(*args)
+}
---
+++ src/test/kotlin/com/kata/board/BoardKataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kata.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardKataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
