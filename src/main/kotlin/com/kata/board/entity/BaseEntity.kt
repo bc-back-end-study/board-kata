13c130227e227729cbe01ceb1a1a427c49675a39
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-11 11:28:44 +0900

    auditing Entity

---
+++ src/main/kotlin/com/kata/board/entity/BaseEntity.kt
@@ -0,0 +1,18 @@
+package com.kata.board.entity
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(value = [AuditingEntityListener::class])
+open class BaseEntity{
+    @CreatedDate
+    private lateinit var createdDate: LocalDateTime
+
+    @LastModifiedDate
+    private lateinit var modifiedDate: LocalDateTime
+}
