f61747c877bbf1122f76c961b9a558dc921368ce
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-11 11:29:52 +0900

    jap audition setting

---
+++ src/main/kotlin/com/kata/board/config/JpaConfiguration.kt
@@ -0,0 +1,9 @@
+package com.kata.board.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfiguration {
+}
