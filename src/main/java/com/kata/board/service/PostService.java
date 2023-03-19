d11c39e8a9488e79b739035d06a175d022aefc21
sunghyun <tjdgus2319@gmail.com>
2023-03-19 12:04:24 +0900

    add PostService

--- src/main/java/com/kata/board/service/PostService.java
+++ src/main/java/com/kata/board/service/PostService.java
@@ -1,2 +1,22 @@
-package com.kata.board.service;public class PostService {
+package com.kata.board.service;
+
+import com.kata.board.dto.PostDto;
+import com.kata.board.entity.Post;
+import com.kata.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private PostRepository postRepository;
+
+    public List<PostDto> getPostList(Long id){
+        return postRepository.findAllBy().stream().map(Post::makeToPostDto)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/test/kotlin/com/kata/board/repository/PostRepositoryTest.java
+++ src/test/kotlin/com/kata/board/repository/PostRepositoryTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kata.board.repository;
+
+import com.kata.board.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 class PostRepositoryTest {
-  
+
+    private final PostRepository postRepository;
+    @Test
+    @DisplayName("게시판 전체 조회 테스트")
+    void findAllPost(){
+        //given
+        List<Post> postList = new ArrayList<>();
+        for (int i=0; i< 100; i++) {
+            Post post = new Post();
+            post.setTitle("test" + i);
+            post.setContent(i+ "테스트입니다");
+            postList.add(post);
+        }
+        postRepository.saveAll(postList);
+        //when
+        List<Post> posts = postRepository.findAll();
+        //then
+        posts.stream().forEach(post -> System.out.println(post.toString()));
+    }
+
 }
