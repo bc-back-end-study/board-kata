06e914f74c8918e4754658c0d80f6275456e9f55
SeungJae Lee <lsj8367@naver.com>
2023-03-19 14:31:31 +0900

    게시판 service layer 구현 (#17)
    * :wrench: h2 설정
    
    * :sparkles: 서비스 구현
    
    * :recycle: 값 검증 상세화
    
    * :recycle: 수정자 변경
    
    * :sparkles: 응답 객체 구현
    
    * :recycle: 접근제어자 수정
    
    * :recycle: 메소드명 수정
    
    * :recycle: 테스트 비밀번호 변경

---
+++ src/main/java/com/kata/board/post/service/PostService.kt
@@ -0,0 +1,28 @@
+package com.kata.board.post.service
+
+import com.kata.board.post.domain.PostReadRepository
+import com.kata.board.post.service.response.PagingResponse
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class PostService(
+    private val postReadRepository: PostReadRepository
+) {
+    fun findAllPagenatedPost(page: Pageable): Page<PagingResponse> {
+        val pagenatedPost = postReadRepository.findAllPagenatedPost(page)
+        return pagenatedPost.map { post ->
+            PagingResponse(
+                post.id!!,
+                post.title,
+                post.content,
+                post.user!!.username,
+                post.view,
+                post.createdDate!!.toLocalDate()
+            )
+        }
+    }
+}
