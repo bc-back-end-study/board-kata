150ec70c8ec83a14811655710ca175ca1c8b60e8
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-03-30 17:26:28 +0900

    Name 변경 및 코드 리펙토링

--- src/main/java/com/kata/board/repository/post/PostRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kata.board.repository.post
-
-import com.kata.board.entity.Post
-
-interface PostRepository {
-    fun getPost(id: Long): Post
-
-    fun getAllPost(): List<Post>
-}
--- src/main/java/com/kata/board/service/PostService.kt
+++
@@ -1,9 +0,0 @@
-package com.kata.board.service
-
-import com.kata.board.dto.PostDto
-
-interface PostService {
-    fun getPost(id: Long): PostDto
-
-    fun getAllPost(): List<PostDto>
-}
