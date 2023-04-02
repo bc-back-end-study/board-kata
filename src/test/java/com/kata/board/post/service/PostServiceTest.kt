4923de10a8daa9c55a6a421f8549995b2922ff4d
lsj8367 <lsj8367@naver.com>
2023-04-02 11:26:59 +0900

    :sparkles: 게시글 등록하기 구현

--- src/test/java/com/kata/board/post/service/PostServiceTest.kt
+++ src/test/java/com/kata/board/post/service/PostServiceTest.kt
@@ -1,9 +1,13 @@
 package com.kata.board.post.service
 
-import com.kata.board.entity.User
+import com.kata.board.user.domain.User
 import com.kata.board.post.domain.Post
 import com.kata.board.post.repository.PostRepository
+import com.kata.board.post.service.request.PostRequest
+import com.kata.board.user.repository.UserRepository
+import com.kata.board.util.BoardBootTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -14,8 +18,15 @@ import org.springframework.data.domain.PageRequest
 @SpringBootTest
 class PostServiceTest(
     @Autowired private val postRepository: PostRepository,
-    @Autowired private val postService: PostService
-) {
+    @Autowired private val postService: PostService,
+    @Autowired private val userRepository: UserRepository
+): BoardBootTest() {
+
+    @AfterEach
+    fun clean() {
+        cleanup!!.execute()
+    }
+
     @Test
     @DisplayName("게시글 페이징 조회 시 정상 반환")
     fun `Should findAllPagenatedPost When getPostAll`() {
@@ -32,6 +43,28 @@ class PostServiceTest(
             assertThat(result.title).isEqualTo("제목")
             assertThat(result.content).isEqualTo("내용")
             assertThat(result.username).isEqualTo("유저명")
+            assertThat(result.viewCount).isZero
+            assertThat(result.createdDate).isNotNull()
+        })
+    }
+
+    @Test
+    @DisplayName("게시글 등록하기")
+    fun `Should register post When user post registration request`() {
+        //given
+        userRepository.save(User("테스트", "password", "별명", "test@email.com"))
+
+        //when
+        postService.registerPost(PostRequest("title", "content", 1L))
+
+        //then
+        val post = postService.findAllPagenatedPost(PageRequest.of(0, 5))
+        val result = post.content[0]
+        assertAll({
+            assertThat(result.id).isEqualTo(1L)
+            assertThat(result.title).isEqualTo("title")
+            assertThat(result.content).isEqualTo("content")
+            assertThat(result.username).isEqualTo("테스트")
             assertThat(result.viewCount).isZero
             assertThat(result.createdDate).isNotNull()
         })
