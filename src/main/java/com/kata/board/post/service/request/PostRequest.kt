f15943b7e448b64bc885537b88e763a6d94b820e
lsj8367 <lsj8367@naver.com>
2023-04-02 11:27:13 +0900

    :sparkles: data class 로 변경

--- src/main/java/com/kata/board/post/service/request/PostRequest.kt
+++ src/main/java/com/kata/board/post/service/request/PostRequest.kt
@@ -3,14 +3,13 @@ package com.kata.board.post.service.request
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
-class PostRequest {
-
+data class PostRequest(
     @NotBlank(message = "제목은 null 일 수 없습니다.")
-    val title: String? = null
+    val title: String? = null,
 
     @NotBlank(message = "내용은 null 일 수 없습니다.")
-    val content: String? = null
+    val content: String? = null,
 
     @NotNull(message = "유저 id는 필수 값입니다.")
     val userId: Long? = null
-}
+)
