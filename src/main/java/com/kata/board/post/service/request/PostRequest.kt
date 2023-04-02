2c774853e53d3344440e612b457398b353f9c59f
lsj8367 <lsj8367@naver.com>
2023-04-02 11:07:57 +0900

    :sparkles: 등록객체 dto 구현

---
+++ src/main/java/com/kata/board/post/service/request/PostRequest.kt
@@ -0,0 +1,16 @@
+package com.kata.board.post.service.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+class PostRequest {
+
+    @NotBlank(message = "제목은 null 일 수 없습니다.")
+    val title: String? = null
+
+    @NotBlank(message = "내용은 null 일 수 없습니다.")
+    val content: String? = null
+
+    @NotNull(message = "유저 id는 필수 값입니다.")
+    val userId: Long? = null
+}
