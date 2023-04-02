d5f1202267715dad525cbe330d64f31bf1a39b71
lsj8367 <lsj8367@naver.com>
2023-04-02 11:53:43 +0900

    :recycle: 필드값 검증하도록 변경

--- src/main/java/com/kata/board/post/service/request/PostRequest.kt
+++ src/main/java/com/kata/board/post/service/request/PostRequest.kt
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class PostRequest(
-    @NotBlank(message = "제목은 null 일 수 없습니다.")
+    @field:NotBlank(message = "제목은 null 일 수 없습니다.")
     val title: String? = null,
 
-    @NotBlank(message = "내용은 null 일 수 없습니다.")
+    @field:NotBlank(message = "내용은 null 일 수 없습니다.")
     val content: String? = null,
 
-    @NotNull(message = "유저 id는 필수 값입니다.")
+    @field:NotNull(message = "유저 id는 필수 값입니다.")
     val userId: Long? = null
 )
