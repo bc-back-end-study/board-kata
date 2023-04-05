596202cd3f29ce9ca86a4295ae1e119e090f45f1
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-04-05 18:45:41 +0900

    modelMapper 추가

--- src/main/kotlin/com/kata/board/repository/post/PostRepositoryImpl.kt
+++ src/main/kotlin/com/kata/board/repository/post/PostRepositoryImpl.kt
@@ -4,12 +4,14 @@ import com.kata.board.dto.PostDto
 import com.kata.board.entity.Post
 import com.kata.board.repository.BoardJpaRepository
 import org.modelmapper.ModelMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Repository
 
 @Repository
 class PostRepositoryImpl (
     private val boardJpaRepository: BoardJpaRepository,
-    private val modelMapper: ModelMapper
+    @Autowired private val modelMapper: ModelMapper
 ): PostRepository {
     override fun getPost(id: Long): Post {
         return boardJpaRepository.findById(id).get()
