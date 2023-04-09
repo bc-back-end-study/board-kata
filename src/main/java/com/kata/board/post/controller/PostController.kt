102f1426e34964a89fd35ec176b137eacfb8c7a8
lsj8367 <lsj8367@naver.com>
2023-04-09 11:41:47 +0900

    :sparkles: 수정기능 api 구현

--- src/main/java/com/kata/board/post/controller/PostController.kt
+++ src/main/java/com/kata/board/post/controller/PostController.kt
@@ -1,12 +1,17 @@
 package com.kata.board.post.controller
 
 import com.kata.board.post.service.PostService
+import com.kata.board.post.service.request.PostUpdateRequest
 import com.kata.board.post.service.response.PagingResponse
+import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,6 +25,12 @@ class PostController(
     fun findAllPagenatedPost(@PageableDefault(size = 10, page = 0) pageable: Pageable): ResponseEntity<Page<PagingResponse>> {
         val findAllPagenatedPost = postService.findAllPagenatedPost(pageable)
         return ResponseEntity.ok(findAllPagenatedPost)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePost(@PathVariable id: Long, @Valid @RequestBody request: PostUpdateRequest): ResponseEntity<Void> {
+        postService.updatePost(id, request)
+        return ResponseEntity.ok().build()
     }
 
 }
--- src/test/java/com/kata/board/post/controller/PostControllerTest.kt
+++ src/test/java/com/kata/board/post/controller/PostControllerTest.kt
@@ -5,6 +5,9 @@ import com.kata.board.post.service.response.PagingResponse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.BDDMockito.`when`
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,24 +15,50 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.put
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import java.time.LocalDate
+import java.util.stream.Stream
 
 @WebMvcTest(controllers = [PostController::class])
 class PostControllerTest {
 
     companion object {
         private const val POST_BASE_URL = "/posts/v1"
+
+        @JvmStatic
+        fun updatePost(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("""
+                        {
+                            "title": "",
+                            "content": "content"
+                        }
+                    """, "제목은 비어있을 수 없습니다."),
+                Arguments.of("""
+                        {
+                            "title": "title",
+                            "content": ""
+                        }
+                    """, "내용은 비어있을 수 없습니다.")
+            )
+        }
     }
 
     @Autowired
     private lateinit var ctx: WebApplicationContext
-
     @MockBean
     private lateinit var postService: PostService
 
@@ -42,6 +71,7 @@ class PostControllerTest {
             .alwaysDo<DefaultMockMvcBuilder>(print())
             .build()
     }
+
 
     @Test
     @DisplayName("게시글 전체 조회를 하는 경우 페이징 처리된 리스트를 반환한다.")
@@ -69,4 +99,19 @@ class PostControllerTest {
         Mockito.any<T>()
         return null as T
     }
+
+    @DisplayName("validation 검증 시 예외 400")
+    @ParameterizedTest
+    @MethodSource("updatePost")
+    fun `Should validaitonException When illegal arguments request`(json: String, errorMessage: String) {
+        mockMvc.perform(
+            put(POST_BASE_URL + "/1").accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json.trimIndent())
+                .characterEncoding(Charsets.UTF_8)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.errorMessage").value(errorMessage))
+    }
+
 }
