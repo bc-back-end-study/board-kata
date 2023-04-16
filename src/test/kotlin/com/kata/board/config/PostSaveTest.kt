feb08d57deb8bb1aeb5b074510b2c6829d3bcde5
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-04-16 12:11:40 +0900

    Get, Delete, Update Test

--- src/test/kotlin/com/kata/board/config/PostSaveTest.kt
+++ src/test/kotlin/com/kata/board/config/PostSaveTest.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDateTime
+import java.util.Random
 
 @SpringBootTest
 class PostSaveTest (
@@ -16,16 +17,12 @@ class PostSaveTest (
 ) {
     @Test
     @DisplayName("게시글을 등록 했을 때 id값이 0보다 커야한다.")
-    fun `Get Save Post Item`() {
+    fun `Save Post Item`() {
 
         //given
         val poseDto = PostDto(
-            id = 0L,
             title = "Test Title",
             content = "Test Content",
-            createdDate = LocalDateTime.now(),
-            modifiedDate = LocalDateTime.now(),
-            view = 0
             )
 
         //when
@@ -33,5 +30,62 @@ class PostSaveTest (
 
         //then
         assertThat(result.id).isGreaterThan(0L)
+    }
+
+    @Test
+    @DisplayName("게시글 조회 했을 경우 검색한 id와 검색한 id 값이 같아야 한다.")
+    fun `Get Post Item`() {
+        //given
+        val beforePosts = postService.getAllPost()
+
+        val result = postService.getPost(beforePosts[0].id?:return)
+
+        assertThat(result.id).isEqualTo(beforePosts[0].id)
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 후 게시글 전체 갯수가 작아져야한다.")
+    fun `Delete Post Item`() {
+        //given
+        val beforePosts = postService.getAllPost()
+
+        //when
+        postService.deletePost(beforePosts[0].id ?: return)
+
+        //then
+        val afterPosts = postService.getAllPost()
+
+        assertThat(beforePosts.size).isGreaterThan(afterPosts.size)
+    }
+
+    @Test
+    @DisplayName("업데이트 이후 게시글 갯수는 같고 내용은 같지 않아야 한다.")
+    fun `Update Post Item`() {
+        //given
+        val beforePosts = postService.getAllPost()
+
+        val poseDto = PostDto(
+            id = beforePosts[0].id,
+            title = "Test Title",
+            content = getRandomString(),
+        )
+
+        //when
+        postService.updatePost(poseDto)
+
+        //then
+        val afterPosts = postService.getAllPost()
+        assertThat(beforePosts.size).isEqualTo(afterPosts.size)
+        assertThat(beforePosts[0].content).isNotEqualTo(afterPosts[0].content)
+
+    }
+
+
+    fun getRandomString() : String {
+        val length = Random().nextInt(100 - 0) + 100
+        val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..length)
+            .map { charset.random() }
+            .joinToString("")
     }
 }
